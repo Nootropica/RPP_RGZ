9e4216f6f53bd9481aebb845bfe212ec0e6a8443
Владислав Печенкин <mogucci2000@list.ru>
2025-12-20 01:20:33 +0700

    update

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ if __name__ == '__main__':
             pass
     
     print(f"Запуск Flask приложения на порте {port}")
-    app.run(debug=False, port=port)
+    app.run(debug=False, host='127.0.0.1', port=port)
